cc26bc2ac43ff40417f04cadadf60c1816738576
Peng Xiao <pengxiao@outlook.com>
2021-12-12 22:31:09 +0800

    fix: remove old unused code

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
 import "@logseq/libs";
-import { logseq as PL } from "../package.json";
 import { registerCommand } from "./register-command";
-
-const magicKey = `__${PL.id}__loaded__`;
 
 function main() {
   const pluginId = logseq.baseInfo.id;
   logseq.hideMainUI();
   console.info(`#${pluginId}: MAIN`);
   registerCommand();
-  // @ts-expect-error
-  top[magicKey] = logseq;
   console.info(`#${pluginId}: MAIN DONE`);
 }
 
